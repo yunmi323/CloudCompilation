b509b970638dc1c2da6b4d1bb73e3de8ce7235c7
yunmi323 <457991027@qq.com>
2020-04-22 23:02:40 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,4 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
+
+#sed -i -e '$a' -e '$a192.168.101.1 log.in' package/base-files/files/etc/hosts
+
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/fw876/helloworldr.git package/helloworld
