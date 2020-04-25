7ed9b5785852445bafda9a3faee3fd670fc4a2a1
yunmi323 <457991027@qq.com>
2020-04-25 11:31:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -13,10 +13,10 @@ sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_gener
 #=================================================
 
 # fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # destan19/OpenAppFilter
-#sed -i '$a src-git lienol https://github.com/destan19/OpenAppFilter' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/destan19/OpenAppFilter' feeds.conf.default
 
 #OR
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
