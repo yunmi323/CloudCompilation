7caba5a9c5ade4ba88c072b22c572a2d07b70648
yunmi323 <457991027@qq.com>
2020-04-25 11:52:02 +0800

    Update and rename diy.sh to diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+# fw876/helloworld
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# destan19/OpenAppFilter
+sed -i '$a src-git lienol https://github.com/destan19/OpenAppFilter' feeds.conf.default
+
