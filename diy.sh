b1eabcb4f4d59a4c09d69f15610dd1c2fd4f320f
yunmi323 <457991027@qq.com>
2020-04-25 11:30:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,7 +10,16 @@ sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_gener
 
 #sed -i -e '$a' -e '$a192.168.101.1 log.in' package/base-files/files/etc/hosts
 
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/fw876/helloworldr.git package/helloworld
-./scripts/feeds update -a
-./scripts/feeds install -a
+#=================================================
+
+# fw876/helloworld
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# destan19/OpenAppFilter
+#sed -i '$a src-git lienol https://github.com/destan19/OpenAppFilter' feeds.conf.default
+
+#OR
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/fw876/helloworldr.git package/helloworld
+#./scripts/feeds update -a
+#./scripts/feeds install -a
