f67ad771851a5890dd3d94ba29ab982ae85b984d
yunmi323 <457991027@qq.com>
2020-05-05 15:51:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,10 @@
 sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_generate
 
 #sed -i -e '$a' -e '$a192.168.101.1 log.in' package/base-files/files/etc/hosts
+
+#Change the default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+
+#Delete default password
+#sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
+
