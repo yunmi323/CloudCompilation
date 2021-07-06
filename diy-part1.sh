046086c1e981b64db3dc2bf30d354fe40cf8785d
yunmi323 <457991027@qq.com>
2021-07-06 11:12:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,8 +7,11 @@
 #=================================================
 
 # fw876/helloworld
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '$a src-git yunmi323 https://github.com/fw876/helloworld' feeds.conf.default
 
 # destan19/OpenAppFilter
 #sed -i '$a src-git lienol https://github.com/destan19/OpenAppFilter' feeds.conf.default
 
+# yunmi323/luci-theme-argon
+sed -i '$a src-git yunmi323 https://github.com/yunmi323/luci-theme-argon' feeds.conf.default
