c60f73d09db6349fe0a87942d0afc401d5fb1a1b
yunmi323 <457991027@qq.com>
2021-07-06 11:15:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@ sed -i 's/192.168.1.1/192.168.101.1/g' package/base-files/files/bin/config_gener
 #sed -i -e '$a' -e '$a192.168.101.1 log.in' package/base-files/files/etc/hosts
 
 #Change the default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-material/g' ./feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 
 #Delete default password
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
