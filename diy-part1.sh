eccc8a8d69160fad8ff020459278257d8720f6eb
yunmi323 <457991027@qq.com>
2021-07-08 14:08:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@ sed -i '$a src-git destan19 https://github.com/destan19/OpenAppFilter' feeds.con
 
 # yunmi323/luci-theme-argon
 #sed -i '$a src-git yunmi323 https://github.com/yunmi323/luci-theme-argon' feeds.conf.default
-rm -rf package/lean/luci-theme-argon
-git clone https://github.com/yunmi323/luci-theme-argon.git package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
+#git clone https://github.com/yunmi323/luci-theme-argon.git package/lean/luci-theme-argon
 
 #京东签到插件及其node-request依赖
 #git clone https://github.com/jerrykuku/node-request  package/node-request 
