d467a8620cd38a13330a95591c4502fcb62b2c80
yunmi323 <457991027@qq.com>
2021-07-08 15:03:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,11 +13,13 @@ sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
 # destan19/OpenAppFilter
 sed -i '$a src-git destan19 https://github.com/destan19/OpenAppFilter' feeds.conf.default
 
+# xiaoqingfengATGH/infinityfreedom
+sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+
 # yunmi323/luci-theme-argon
 #sed -i '$a src-git yunmi323 https://github.com/yunmi323/luci-theme-argon' feeds.conf.default
-rm -rf package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
 #git clone https://github.com/yunmi323/luci-theme-argon.git package/lean/luci-theme-argon
-git clone - b lede https://github.com/yunmi323/luci-theme-argon.git package/luci-theme-argon
 
 #京东签到插件及其node-request依赖
 #git clone https://github.com/jerrykuku/node-request  package/node-request 
